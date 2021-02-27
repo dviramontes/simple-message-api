4d299c9f27eaa05003dbcae5ffcb3169ed43a41f
dviramontes <dviramontes@gmail.com>
2021-02-26 22:40:21 -0700

    express dep + /ping health-check route

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.get("/ping", (req, res) => res.send("pong"));
+
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
